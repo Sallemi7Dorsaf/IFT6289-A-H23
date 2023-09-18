422c59aa06e93723a08b89ab09a7389cdef0d856
Dorsaf Sallami <37942992+Sallemi7Dorsaf@users.noreply.github.com>
2023-09-18 13:51:37 -0400

    Add files via upload

---
+++ IFT6289_H23_A1/run.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import random
+import sys
+import time
+
+import numpy as np
+
+from sgd import sgd
+from data_process import A1dataset
+from word2vec import word2vec_sgd_wrapper, skipgram, cbow, neg_sampling_loss_and_gradient
+
+# Check Python Version
+assert sys.version_info[0] == 3
+assert sys.version_info[1] >= 5
+
+# Reset the random seed to make sure that everyone gets the same results
+random.seed(42)
+np.random.seed(6289)
+
+dataset = A1dataset()
+tokens = dataset.tokens()  # word2Idx
+n_words = len(tokens)
+
+# We are going to train 10-dimensional vectors for this assignment
+dim_vectors = 10
+
+# Context size
+C = 5
+
+print("Skip-gram")
+startTime = time.time()
+word_vectors = np.concatenate(
+    ((np.random.rand(n_words, dim_vectors) - 0.5) /
+     dim_vectors, np.zeros((n_words, dim_vectors))),
+    axis=0)
+
+word_vectors = sgd(
+    lambda vec: word2vec_sgd_wrapper(skipgram, tokens, vec, dataset, C,
+                                     neg_sampling_loss_and_gradient),
+    word_vectors, 0.2, 35000, tokens, None, True, PRINT_EVERY=10, model="skip-gram")
+
+print("sanity check: cost at convergence should be around or below 9")
+print("training took %d seconds" % (time.time() - startTime))
+print("="*20)
+
+print("CBOW:")
+startTime = time.time()
+word_vectors = np.concatenate(
+    ((np.random.rand(n_words, dim_vectors) - 0.5) /
+     dim_vectors, np.zeros((n_words, dim_vectors))),
+    axis=0)
+
+word_vectors = sgd(
+    lambda vec: word2vec_sgd_wrapper(cbow, tokens, vec, dataset, C,
+                                     neg_sampling_loss_and_gradient),
+    word_vectors, 0.2, 35000, tokens, None, True, PRINT_EVERY=10, model="CBOW")
+
+print("sanity check: cost at convergence should be around or below 9")
+print("training took %d seconds" % (time.time() - startTime))
